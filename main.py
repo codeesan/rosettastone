644fdc2d639208f4119b51549ea0d1012026156e
jeffersoncody <jeffersoncody@icloud.com>
2025-01-25 11:13:10 -0800

    hi cody v.1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import click
 eng = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"," ",".",","]
 hyro = ["N","D","E","F","S","I","W","V","O","P","X","T","Y","A","C","G","K","M","U","H","J","L","Z","Q","B","R"," ",".",","]
 
@@ -32,4 +33,20 @@ def hyro2english(mystring):
         decoded_value += engletter
     print(decoded_value)
 
+@click.command()
+@click.option("--mytype", "-t", prompt="eng2hyro or hyro2eng", help="English to Hyrogliphics (eng2hyro) or Hyrogliphics to English (hyro2eng)")
+@click.option("--phrase", "-p", prompt="enter a phrase to encode or decode", help="Phrase can be any letters include '.', ',' and ' '.")
 
+def rosettastone(mytype,phrase):
+    allowed_types = ['eng2hyro', 'hyro2eng']
+    if mytype not in allowed_types:
+        print("error: you must enter eng2hyro or hyro2eng!")
+        exit()
+    if mytype == "eng2hyro":
+        eng2hyro(phrase)
+    elif mytype == "hyro2eng":
+        hyro2english(phrase)
+    
+    
+if __name__ == '__main__':
+    rosettastone()
